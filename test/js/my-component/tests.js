c0c0d3d6693d7031bce43809fcccb07c961908de
Evan Goer <evan@goer.org>
2013-10-15 14:26:39 -0700

    Add working mocha test suite skeleton

---
+++ test/js/my-component/tests.js
@@ -0,0 +1,23 @@
+/*global describe, it, beforeEach, afterEach, expect, appendDiv */
+
+describe('The appendDiv() function', function () {
+    var container = document.getElementById('test');
+
+    /*
+     * Teardown function so that the two tests don't interfere
+     */
+    afterEach(function () {
+        container.removeChild(container.lastChild);
+    });
+
+    it('can append a div into a container element', function () {
+        expect(container.lastChild).to.not.be.ok();
+        appendDiv('#test');
+        expect(container.lastChild.tagName).to.be.eql('DIV');
+    });
+
+    it('returns the div it just appended', function () {
+        var div = appendDiv('#test');
+        expect(container.lastChild).to.be.eql(div);
+    });
+});
