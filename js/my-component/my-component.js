349b119e2b6671defb38a66c60e390004ac4043d
Evan Goer <evan@goer.org>
2014-01-15 22:24:32 -0800

    Clarify code under test

--- js/my-component/my-component.js
+++ js/my-component/my-component.js
@@ -2,6 +2,8 @@
  * A trivial snippet of code to put under test.
  */
 function appendDiv(selector) {
-    var element = document.querySelector(selector);
-    return element.appendChild(document.createElement('div'));
+    var container = document.querySelector(selector),
+        div = document.createElement('div');
+
+    return container.appendChild(div);
 }
